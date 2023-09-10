527a1cd0f6a269966ffb62660f5648aaca12e2b9
Samir Isagov <samir.isagov@hotmail.com>
2023-09-10 22:55:19 +0200

    Add gRPC client

---
+++ grpc/GrpcSample.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using GrpcSample.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7227");
+var client = new Greeter.GreeterClient(channel);
+
+var response = client.SayHello(new HelloRequest() { Name = "Sam" });
+
+Console.WriteLine(response.Message);
