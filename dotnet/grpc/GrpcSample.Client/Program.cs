62315a5b30df4e39809f46b48771834eaa7fc34c
smrsgv <samir.isagov@gmail.com>
2024-11-19 21:15:12 +0100

    delete grpc

--- dotnet/grpc/GrpcSample.Client/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Grpc.Net.Client;
-using GrpcSample.Client;
-
-var channel = GrpcChannel.ForAddress("https://localhost:7227");
-var client = new Greeter.GreeterClient(channel);
-
-var response = client.SayHello(new HelloRequest() { Name = "Sam" });
-
-Console.WriteLine(response.Message);
--- dotnet/grpc/GrpcSample.Server/Models/ToDoItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GrpcSample.Server.Models;
-
-public class ToDoItem
-{
-    public int Id { get; set; }
-    public string? Title { get; set; }
-    public string? Description { get; set; }
-    public string Status { get; set; } = "NEW";
-}
